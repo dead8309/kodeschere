bc90e0800a32a0d8889ae5669973b1de9e9a4a71
Vaibhav <68665948+dead8309@users.noreply.github.com>
2024-03-17 12:24:41 +0530

    add user section

---
+++ src/app/control/_components/Dashboard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Home from "./Home";
+import User from "./User";
+
+const DashboardPage = ({ data }: { data: any }) => {
+  return <div className="container flex flex-col space-y-8 mt-10">
+    <User />
+    <Home />
+  </div>
+};
+
+export default DashboardPage;
